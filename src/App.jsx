d361b4fde96350b3b9487a2970cb640b3bcad40b
Trainwagon <pangeranmlg278@gmail.com>
2025-11-29 15:56:04 +0700

    add login page layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import CrosscheckLaporan from './pages/CrosscheckLaporan'
 import RekapitulasiLaporan from './pages/RekapitulasiLaporan'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/crosscheck" element={<CrosscheckLaporan />} />
         <Route path="/rekapitulasi" element={<RekapitulasiLaporan />} />
